7ab176a54f375098f2bbcd85f6904e183e620c3c
anibaltorresp <anibal.torresp@gmail.com>
2025-03-10 21:17:28 -0500

    securityconfig terminado

--- src/main/java/pe/edu/idat/demo_security_jwt/security/FiltroJWTAuthorization.java
+++ src/main/java/pe/edu/idat/demo_security_jwt/security/FiltroJWTAuthorization.java
@@ -1,14 +1,11 @@
 package pe.edu.idat.demo_security_jwt.security;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,30 +17,56 @@ import java.util.stream.Collectors;
 public class FiltroJWTAuthorization extends OncePerRequestFilter {
 
     private final String KEY = "@idat2025";
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        try{
+            if(validarUsoDeToken(request)){
+                Claims claims = validarToken(request);
+                if(claims.get("Authorities") != null){
+                    cargarAutorizaciones(claims);
+                }else
+                    SecurityContextHolder.clearContext();
+            }else {
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        }catch (ExpiredJwtException | UnsupportedJwtException
+                | MalformedJwtException ex){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            ((HttpServletResponse)response).sendError(
+                    HttpServletResponse.SC_FORBIDDEN,
+                    ex.getMessage());
+        }
     }
 
     private void cargarAutorizaciones(Claims claims){
-        List<String> autorizaciones = (List<String>) claims.get("Authorities");
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                claims.getSubject(),
-                null,
-                autorizaciones.stream()
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList()));
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-
+        List<String> autorizaciones = (List<String>)
+                claims.get("Authorities");
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(
+                        claims.getSubject(),
+                        null,
+                        autorizaciones.stream()
+                                .map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toList()));
+        SecurityContextHolder.getContext().setAuthentication(
+                authToken);
     }
+
     private Claims validarToken(HttpServletRequest request){
-        String token = request.getHeader("Authorization").replace("Bearer ","");
-        return Jwts.parser().setSigningKey(KEY.getBytes()).parseClaimsJws(token).getBody();
+        String token = request.getHeader("Authorization")
+                .replace("Bearer ", "");
+        return Jwts.parser().setSigningKey(KEY.getBytes())
+                .parseClaimsJws(token).getBody();
     }
 
     private boolean validarUsoDeToken(HttpServletRequest request){
         String token = request.getHeader("Authorization");
-        if (token == null || token.startsWith("Bearer ")){
+        if(token == null || !token.startsWith("Bearer ")){
             return false;
         }
         return true;
--- src/main/java/pe/edu/idat/demo_security_jwt/security/SecurityConfig.java
+++ src/main/java/pe/edu/idat/demo_security_jwt/security/SecurityConfig.java
@@ -2,13 +2,19 @@ package pe.edu.idat.demo_security_jwt.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import pe.edu.idat.demo_security_jwt.service.impl.DetalleUsuarioService;
 
 @EnableWebSecurity
@@ -16,6 +22,21 @@ import pe.edu.idat.demo_security_jwt.service.impl.DetalleUsuarioService;
 @Configuration
 public class SecurityConfig {
     private final DetalleUsuarioService detalleUsuarioService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http)throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers(HttpMethod.GET,"api/v1/auth/login")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).authenticationProvider(getAuthenticationProvider())
+                .addFilterBefore(new FiltroJWTAuthorization(),
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
     public SecurityConfig(DetalleUsuarioService detalleUsuarioService) {
         this.detalleUsuarioService = detalleUsuarioService;
