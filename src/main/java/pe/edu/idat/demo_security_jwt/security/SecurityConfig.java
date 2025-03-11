9d2ca72ed2711653393246f43c3137a0fc28a0d5
anibaltorresp <anibal.torresp@gmail.com>
2025-03-10 19:49:40 -0500

    Configuracion Segurity

--- src/main/java/pe/edu/idat/demo_security_jwt/security/SecurityConfig.java
+++ src/main/java/pe/edu/idat/demo_security_jwt/security/SecurityConfig.java
@@ -1,4 +1,44 @@
 package pe.edu.idat.demo_security_jwt.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pe.edu.idat.demo_security_jwt.service.impl.DetalleUsuarioService;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
 public class SecurityConfig {
+    private final DetalleUsuarioService detalleUsuarioService;
+
+    public SecurityConfig(DetalleUsuarioService detalleUsuarioService) {
+        this.detalleUsuarioService = detalleUsuarioService;
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider getAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(detalleUsuarioService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager(
+        AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
